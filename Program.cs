96175f9aef3e9f01fd52b18d2af4e4f9248a8ac0
Manav Patel <manav.patel4251@gmail.com>
2021-09-16 15:29:11 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exam
+{
+    class MainClass
+    {
+        public static int OddCounts(int[] numbers)
+        {
+            int count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    count += 1;
+                }
+                else
+                {
+                    count += 0;
+                }
+
+            }
+            Console.WriteLine("The Count is:- " + count);
+            return count;
+        }
+        public static void Main(string[] args)
+        {
+
+            int[] numbers = { 4, 3, 6, 7, 5, 6 };
+            OddCounts(numbers);
+
+
+        }
+    }
+}
